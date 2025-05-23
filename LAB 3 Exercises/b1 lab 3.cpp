714b4b7d7e8b1e56be19edb9b31045b637901982
huyoniichannuwu <damhungbui@gmail.com>
2025-05-23 10:14:24 +0700

    Add files via upload

---
+++ LAB 3 Exercises/b1 lab 3.cpp
@@ -0,0 +1,24 @@
+﻿// lab 3-ham-bai 1 sua.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Viết hàm tìm số đảo ngược của 1 số nguyên vd: 123 -> 321
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+void reverse(int x) 
+{
+	int n;
+	while (x != 0) {
+		n = x % 10;
+		x = x / 10;
+		cout << n;                // in n thi ta se duoc nghiem cua ham
+	}
+}
+
+
+int main()
+{
+	int x;
+	cout << "Nhap mot so nguyen de dao nguoc"; cin >> x;
+	//ap dung ham
+	reverse(x);
+}
